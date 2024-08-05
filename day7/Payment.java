b9ceee01f9563bb09336ba4dc72406ecf9d60032
LAVANYA G <125266121+Lavanya250@users.noreply.github.com>
2024-08-05 11:11:34 +0530

    crud operation

---
+++ day7/Payment.java
@@ -0,0 +1,59 @@
+// package com.supermarket.freshmart.model;
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.Data;
+
+// @Entity
+// @Data
+// @Table(name="payment_table")
+// public class Payment {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     private String paymentMethod;
+// }
+
+// package com.supermarket.freshmart.model;
+
+// import jakarta.persistence.*;
+// import lombok.Data;
+
+// @Entity
+// @Data
+// @Table(name="payment_table")
+// public class Payment {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     private String paymentMethod;
+
+//     @OneToOne(mappedBy = "payment")
+//     private Order order;
+// }
+
+package com.supermarket.freshmart.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "payment_table")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String paymentMethod;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+}
---
+++ day7/Product.java
@@ -0,0 +1,43 @@
+// package com.supermarket.freshmart.model;
+
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import lombok.Data;
+
+// @Entity
+// @Data
+// public class Product {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     private String name;
+//     private String imageUrl;  // Field for the image URL
+//     private double price;
+//     private int amount;
+//     private int rating;
+// }
+
+package com.supermarket.freshmart.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imageUrl;  // Field for the image URL
+    private double price;
+    private int amount;
+    private int rating;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders;
+}
